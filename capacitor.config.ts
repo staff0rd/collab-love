337f788e36b08fedb0460870854db550d4d082eb
Stafford Williams <stafford.williams@gmail.com>
2026-07-06 22:49:29 +1000

    Add Capacitor iOS platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "love.collab.app",
+  appName: "collab-love",
+  webDir: "dist",
+};
+
+export default config;
